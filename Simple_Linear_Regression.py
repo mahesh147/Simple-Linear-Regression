291b9ceec1b860031c0fa036a9d71baa31d4979b
Rajshekhar Rakshit <44888331+DextroLaev@users.noreply.github.com>
2020-09-30 18:05:54 +0530

    Update Simple_Linear_Regression.py

--- Simple_Linear_Regression.py
+++ Simple_Linear_Regression.py
@@ -4,44 +4,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
 
-# Importing the datasets
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+class Linear_regression:
+	
+	def __init__(self,train_data,train_labels):
+		self.train_data = train_data
+		self.train_labels = train_labels
+		self.new_train_data = np.insert(self.train_data,0,1,axis=1)		
+		self.weights = np.zeros((2,1))		
+		self.epochs = 1500
+		self.alpha = 0.01
+
+	def hypothesis(self):
+		return np.dot(self.new_train_data,self.weights)	
+		
+	def cost(self):
+		cost = (1/(2*np.size(self.train_labels)))*np.sum((self.hypothesis()-self.train_labels)**2)
+		return cost		
+
+	def derivative(self):
+		return (1/np.size(self.train_labels))*np.dot(self.new_train_data.T,(self.hypothesis()-self.train_labels))
+
+	def train(self):
+		self.loss = []		
+		for i in range(self.epochs):
+			cost = self.cost()					
+			self.weights = self.weights - (self.alpha) * self.derivative()
+			self.loss.append(cost)
+		
+		plt.plot(self.loss)
+		plt.show()	
+		return self.weights,np.array(self.loss)
+		
+	def predict(self,data):
+		return np.dot(data,self.weights)	
+
+	def visualize(self,data):
+		data = self.hypothesis()
+		plt.xlabel('population of city in 10,000s')
+		plt.ylabel('profit in $10,000')		
+		plt.scatter(self.train_data,self.train_labels,marker='x',color='red',label='Training data')
+		plt.plot(self.new_train_data[:,1],data,label='Linear regression')
+		plt.legend(loc='lower right')
+		plt.show()						
+
+if __name__ == '__main__':
  
-datasets = pd.read_csv('Salary_Data.csv')
+ # Reading data from csv file
+ 
+	data = pd.read_csv('ex1data1.txt')
+	train_data = np.array(data.iloc[:,:1])
+	train_labels = np.array(data.iloc[:,1:])			
 
-X = datasets.iloc[:, :-1].values
-Y = datasets.iloc[:, 1].values
-
-# Splitting the dataset into the Training set and Test set
-
-from sklearn.model_selection import train_test_split
-X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
-
-# Fitting Simple Linear Regression to the training set
-
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_Train, Y_Train)
-
-# Predicting the Test set result ￼
-
-Y_Pred = regressor.predict(X_Test)
-
-
-# Visualising the Training set results
-
-plt.scatter(X_Train, Y_Train, color = 'red')
-plt.plot(X_Train, regressor.predict(X_Train), color = 'blue')
-plt.title('Salary vs Experience  (Training Set)')
-plt.xlabel('Years of experience')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the Test set results
-
-plt.scatter(X_Test, Y_Test, color = 'red')
-plt.plot(X_Train, regressor.predict(X_Train), color = 'blue')
-plt.title('Salary vs Experience  (Training Set)')
-plt.xlabel('Years of experience')
-plt.ylabel('Salary')
-plt.show()
+ # Applying linear regression
+  
+	gd = Linear_regression(train_data,train_labels)	
+	print('older cost: ',gd.cost())
+	result = gd.train()
+	print('updated theta: \n',result[0])
+	print('final cost: ',gd.cost())
+	gd.visualize(gd.hypothesis())	
